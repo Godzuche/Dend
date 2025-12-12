89dd4bf7d28f88fbc3ea2edb980ef36d8eeb4f47
godzuche <godxuche@gmail.com>
2025-12-12 10:55:23 +0100

    add privacy message to the welcome message

--- app/src/main/java/com/godzuche/dend/features/onboarding/components/WelcomePage.kt
+++ app/src/main/java/com/godzuche/dend/features/onboarding/components/WelcomePage.kt
@@ -4,14 +4,18 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,13 +41,13 @@ import com.godzuche.dend.ui.theme.DendTheme
 
 @Composable
 fun WelcomePage(
-    onGetStartedClick: () -> Unit
+    onGetStartedClick: () -> Unit,
 ) {
     var startAnimation by remember { mutableStateOf(false) }
     val alphaAnim by animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0f,
         animationSpec = tween(durationMillis = 1000),
-        label = "alphaAnimation"
+        label = "alphaAnimation",
     )
 
     LaunchedEffect(Unit) {
@@ -57,10 +62,11 @@ fun WelcomePage(
                 .padding(horizontal = 32.dp)
                 .alpha(alphaAnim),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
+            Spacer(Modifier.weight(1f))
+
             val composition by rememberLottieComposition(
-                spec = LottieCompositionSpec.RawRes(R.raw.branded_call_access_toggle_animation)
+                spec = LottieCompositionSpec.RawRes(R.raw.branded_call_access_toggle_animation),
             )
             LottieAnimation(
                 composition = composition,
@@ -68,26 +74,45 @@ fun WelcomePage(
                 modifier = Modifier.size(250.dp),
             )
 
-            Spacer(Modifier.height(48.dp))
+            Spacer(Modifier.height(32.dp))
 
             Text(
 //                text = "Reclaim Your Focus.",
                 text = "Find Your Focus.",
                 style = MaterialTheme.typography.displaySmall,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
             )
 
             Spacer(Modifier.height(16.dp))
 
             Text(
-//                text = "DenD acts as your personal call firewall, silently blocking unwanted calls so you can stay in the zone.",
                 text = "DenD is your personal switch for peace and quiet. Silence unwanted calls and protect your flow.",
                 style = MaterialTheme.typography.bodyLarge,
                 textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
             )
 
-            Spacer(Modifier.height(64.dp))
+            Spacer(Modifier.height(24.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Lock,
+                    contentDescription = "Privacy Icon",
+                    modifier = Modifier.size(16.dp),
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+                Text(
+                    text = "Offline and Private by Design",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
+            }
+
+            Spacer(Modifier.weight(1f))
 
             Button(
                 onClick = onGetStartedClick,
@@ -95,24 +120,23 @@ fun WelcomePage(
                     .fillMaxWidth()
                     .height(56.dp),
                 shape = MaterialTheme.shapes.extraLarge,
-                elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp)
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 4.dp),
             ) {
                 Text(
-//                    text = "Get Started",
                     text = "Begin",
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
                 )
             }
+
+            Spacer(modifier = Modifier.height(40.dp))
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_6")
 @Composable
 private fun WelcomePagePreview() {
     DendTheme {
-        WelcomePage(
-            onGetStartedClick = {}
-        )
+        WelcomePage(onGetStartedClick = {})
     }
 }
